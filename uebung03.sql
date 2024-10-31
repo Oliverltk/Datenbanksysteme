30def4d0ad8a88c357a2b09316dc2f46f95ab77d
dabis <oliver.litke2002@gmail.com>
2024-10-31 10:35:31 +0100

    Uploading Übung03

--- uebung03.sql
+++ uebung03.sql
@@ -5,14 +5,15 @@ Create view
 select
     people.id as people_id,
     people.name as people_name,
-    people.planet as people_planet,
+    planet.name as planet_name,
     starship_journeys.journey_start as journey_start,
     starship_journeys.journey_end as journey_end,
     starships.name as starship_name,
     starship_journeys.freight_content as freight_content
 from
     people
-    join starships_journeys ON (people.id = starship_journeys.people_id)
-    join starships ON (starships.id = starship_journeys.starship_id) WHERE freight = true;
+    join starship_journeys ON (people.id = starship_journeys.people_id)
+    join starships ON (starships.id = starship_journeys.starship_id) 
+    join planet ON (people.planet_id = planet.id) WHERE starship_journeys.freight = true;
 
 -- Aufgabe 2
