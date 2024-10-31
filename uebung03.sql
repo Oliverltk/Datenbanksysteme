7d2d947b54986e4a566f3824c69182e64687077e
unknown <oliver.litke2002@gmail.com>
2024-10-31 09:05:38 +0100

    Uploading Übung03

---
+++ uebung03.sql
@@ -0,0 +1,14 @@
+-- Aufgabe 1
+
+create view
+    persons_with_freight as
+select
+    people.id as people_id,
+    people.name as people_name,
+    people.planet as people_planet,
+    starship_journeys.journey_start as journey_start,
+    starship_journeys.journey_end as journey_end,
+    starships.name as starship_name
+from
+    people
+    join starships_journeys 
